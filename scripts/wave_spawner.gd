1f3f42be907636fd2756109298d25bb358dae5ab
JonathanHaws <jonathanmhaws1@gmail.com>
2026-03-19 13:21:19 -0600

    getting rid of the auto progression in favor of discrete waves which make engagments a lot simpler and easier to balance (less finicky

--- scripts/wave_spawner.gd
+++ scripts/wave_spawner.gd
@@ -1,6 +1,5 @@
 extends AnimationPlayer
-var cooldown
-var spawner
+@onready var spawner = $SceneSpawner
 
 @export_group("Spawning")
 @export var min_separation: float = 25.0 # Spawns boats fruther if overlapping
@@ -47,18 +46,6 @@ func spawn_boat_out_of_view(
 	boat.cannon_random_cooldown = _cannon_random_cooldown
 	boat.cannon_random_aim = _cannon_aim
 
-@export_group("Base Difficulty")	
-@export var min_ships: float = 0.0
-@export var breathing_time: float = 7
-@export var random_breathing_time: float = 2
-@export var spawn_distance: float = 50.0
-@export var random_spawn_distance: float = 20.0
-@export var speed: float = 400.0
-@export var steer: float = 80.0
-@export var cannon_cooldown: float = 8.0
-@export var cannon_random_cooldown: float = 4.0
-@export var cannon_random_aim: Vector2 = Vector2(5, 5)
-
 @export_group("Waves")	
 @export var next_wave_pool: Array = []
 func is_wave_cleared() -> bool:
@@ -78,9 +65,6 @@ func poll_next_wave() -> void:
 @export_group("Auto Progress Difficulty")
 @export var auto_progress: bool = false
 @export var curve_base_time: float = 100
-@export var curve_min_ships: Curve
-@export var curve_breathing_time: Curve
-@export var curve_random_breathing_time: Curve
 @export var curve_spawn_distance: Curve
 @export var curve_random_spawn_distance: Curve
 @export var curve_speed: Curve
@@ -92,36 +76,7 @@ var progress_time : float = 0.0
 func get_curve_value_at_time(curve: Curve) -> float:
 	var t = clamp(progress_time / curve_base_time, 0.0, 1.0)
 	return curve.sample(t)
-func update_auto_progress(delta: float) -> void:
-	if not auto_progress: return
-	progress_time += delta
-	
-	if curve_min_ships: min_ships = get_curve_value_at_time(curve_min_ships)
-	if curve_breathing_time: breathing_time = get_curve_value_at_time(curve_breathing_time)
-	if curve_random_breathing_time: random_breathing_time = get_curve_value_at_time(curve_random_breathing_time)
-	if curve_spawn_distance: spawn_distance = get_curve_value_at_time(curve_spawn_distance)
-	if curve_random_spawn_distance: random_spawn_distance = get_curve_value_at_time(curve_random_spawn_distance)
-	if curve_speed: speed = get_curve_value_at_time(curve_speed)
-	if curve_steer: steer = get_curve_value_at_time(curve_steer)
-
-func _ready() -> void:
-	cooldown = breathing_time + (randf() * random_breathing_time)
-	spawner = $SceneSpawner
-	#print('test')
 
 func _process(_delta):
-	
-	#print(get_tree().get_nodes_in_group("boats").size())
-	
+
 	poll_next_wave()
-	
-	update_auto_progress(_delta)
-			
-	#print(min_ships)
-	var boat_count = get_tree().get_nodes_in_group("boats").size() - 1
-	if boat_count >= floor(min_ships): return
-	
-	cooldown -= _delta
-	if cooldown > 0: return 
-	cooldown = breathing_time + (randf() * random_breathing_time)
-	spawn_boat_out_of_view()
